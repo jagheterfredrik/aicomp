51ba5221d5300efb4d5741f83c9efbada387d4b5
Fredrik Gustafsson <frgustaf@kth.se>
2013-02-22 11:15:14 +0100

    Add support for multiplayer
    Game master now works on lists of player paths.

--- game_master.py
+++ game_master.py
@@ -40,44 +40,42 @@ class GameMaster(object):
     The .tar should contain a manifest.json in the root
     directory.
     """
-    def __init__(self, player1, player2):
-        self.player1 = player1 
-        self.player2 = player2
+    def __init__(self, players):
+        self.players = players
     
-        p1_tar = tarfile.open(self.player1)
-        p2_tar = tarfile.open(self.player2)
-        p1_tar.extractall(DIR_PREFIX + "/1")
-        p2_tar.extractall(DIR_PREFIX + "/2")
+        for i, player in enumerate(self.players):
+            player_tar = tarfile.open(player)
+            player_tar.extractall(os.path.join(DIR_PREFIX, "/%d"%(i,)))
 
-        config_paths = [DIR_PREFIX+"/1/manifest.json",DIR_PREFIX+"/2/manifest.json"]
+        config_paths = [os.path.join(DIR_PREFIX, "/%d/manifest.json"%(i)) for i in xrange(len(self.players))]
+
         files = []
         # open the config files
         for p in config_paths:
             files.append(open(p))
         # JSON parse them
-        configs = map(json.load,files)
+        configs = map(json.load, files)
         # and close them
         for f in files:
             f.close()
 
         # make the players talk!
-        player1_bin_args = ["python",os.getcwd()+"/"+DIR_PREFIX+"/1/" + configs[0]['executable']]
-        player2_bin_args = ["python",os.getcwd()+"/"+DIR_PREFIX+"/2/" + configs[1]['executable']]
+        players_bin_args = [["python", os.path.join(os.getcwd(), DIR_PREFIX, "/%d/%s"%(i, configs[i]['executable'],))]
+                            for i in xrange(len(self.players))]
         
-        self.p1 = ProcessHandler(player1_bin_args)
-        self.p2 = ProcessHandler(player2_bin_args)
+        self.processes = [ProcessHandler(pba) for pba in players_bin_args]
 
-class guessTheNumberMaster(GameMaster):
-    def __init__(self, p1, p2):
-        GameMaster.__init__(self,p1,p2)
+class GuessTheNumberMaster(GameMaster):
+    def __init__(self, players):
+        GameMaster.__init__(self, players)
     
     def playMatch(self):
         # p1 thinks about a number
-        self.p1.communicate("think\n")
-        number = int(self.p1.readline())
+        self.processes[0].communicate("think\n")
+        number = int(self.processes[0].readline())
         
         # p2 tries to guess the number
-        self.p2.communicate("guess\n")
-        guessed = int(self.p2.readline())
+        self.processes[1].communicate("guess\n")
+        guessed = int(self.processes[1].readline())
 
         print "thought about " + str(number) + ", guessed " + str(guessed)
